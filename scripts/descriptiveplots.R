e25b431bf613a41e3bb82278242e18e9f63b569e
Katherine Hébert <katherine.ahebert@gmail.com>
2020-03-24 16:07:43 -0400

    move to scripts folder

--- scripts/descriptiveplots.R
+++ scripts/descriptiveplots.R
@@ -11,10 +11,21 @@ responses <- read_csv("Documents/GitHub/qcbs_surveyresults/data/responses.csv")
 questions <- colnames(responses)
 
 ## level of study
-levelstudy = table(responses[,questions[4]])
-as.vector(levelstudy)
 
-nrow(responses)
+# summarise results
+levelstudy = table(responses[,questions[4]])
+
+## come back to this to make it people icons!
+
+  # # get fonts for pictochart
+  # # downloaded free FontAwesome fonts (for web) and installed .ttf files
+  # font_import()
+  # # check that Font Awesome is imported
+  # fonts()[grep("Awesome", fonts())]
+  # # this should be fine for Mac OSX
+  # loadfonts()
+
+# waffle plot!
 waffle(levelstudy, rows = 12, title = "Level of study", use_glyph = "user",
        colors = c("#15105c","#c61558","#ffa600"), size = 0.3)
 
